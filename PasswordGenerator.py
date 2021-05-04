6fb18a25db6598014c78f5e1e7f4a395a807b6ae
Kivanc Aykac <kivanc.aykac@mail.utoronto.ca>
2021-05-05 00:59:45 +0300

    update
    more features

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -12,28 +12,75 @@ import string
 length = int(input("Length of the password to be generator? "))
 
 password = ""
-possible = []
-possible[:0] = string.printable  # lots of characters here
+to_use = []
+to_use[:0] = string.printable  # lots of characters here
 
+alphanum = True
+while alphanum:
+    reply = input("Do you want only alpha-numeric characters?(y/n)\n")
+    if reply=='y':
+        break
+    elif reply=='n':
+        alphanum = False
+        break
+    else:
+        print("Please enter either 'y' or 'n'.")
+        continue
+   
+addition = True
+while addition:
+    reply = input("Do you want to add any possible non-English characters?\
+(y/n)\n")
+    if reply=='y':
+        break
+    elif reply=='n':
+        addition = False
+        break
+    else:
+        print("Please enter either 'y' or 'n'.")
+        continue
 
-to_use = possible.copy()  # final string to use
+unwanted = True
+while unwanted:
+    print(to_use)
+    reply = input("Are there any unwanted characters from the above list?\
+(y/n)\n")
+    if reply=='y':
+        break
+    elif reply=='n':
+        unwanted = False
+        break
+    else:
+        print("Please enter either 'y' or 'n'.")
+        continue
 
 scan = True
 again = False
-
 while scan:
-    notpossible = []
-    notpossible[:0] = input("Enter unwanted elements from the below list:\n\
-{}\n".format(possible))  # unwanted ones
-    for locn, value in enumerate(notpossible):
-        try:
-            possible.index("{}".format(value))
-        except:
-            print(value+" is not in the string that was provided.\
-Please enter the unwanted values again.")
-            again = True
-        else:
-            to_use.remove("{}".format(value))
+    if alphanum:
+        to_use = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b',
+                  'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                  'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                  'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                  'Y', 'Z']
+    if addition:
+        non_eng = []
+        non_eng[:0] = input("Enter the wanted non-English characters.\n")
+        to_use += non_eng
+    if unwanted:
+        notpossible = []
+        notpossible[:0] = input("Enter unwanted elements from the below list:\n\
+    {}\n".format(to_use))  # unwanted ones
+        for locn, value in enumerate(notpossible):
+            try:
+                to_use.index("{}".format(value))
+            except:
+                print(value+" is not in the string that was provided.\
+    Please enter the unwanted values again.")
+                again = True
+            else:
+                to_use.remove("{}".format(value))
     if again:
         pass  # redo the scan
     else:
